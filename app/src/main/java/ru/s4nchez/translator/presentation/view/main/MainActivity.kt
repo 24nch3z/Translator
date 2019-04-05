2fbbcb234ea73651c1211151a63998006e0f9238
24nch3z <sanchezzzgg22@gmail.com>
2019-04-05 10:10:55 +0300

    Добавил вьюшку для отображения отсутствия интернет соединения

--- app/src/main/java/ru/s4nchez/translator/presentation/view/main/MainActivity.kt
+++ app/src/main/java/ru/s4nchez/translator/presentation/view/main/MainActivity.kt
@@ -1,11 +1,24 @@
 package ru.s4nchez.translator.presentation.view.main
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.net.ConnectivityManager
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 import ru.s4nchez.translator.R
 import ru.s4nchez.translator.presentation.view.translator.TranslatorFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private val networkStatusChangeReceiver: BroadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent?) {
+            internet_disable.visibility = if (isInternetConnected()) View.GONE else View.VISIBLE
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,5 +29,23 @@ class MainActivity : AppCompatActivity() {
                     .replace(R.id.container, TranslatorFragment())
                     .commit()
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val intentFilter = IntentFilter()
+        intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE")
+        registerReceiver(networkStatusChangeReceiver, intentFilter)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        unregisterReceiver(networkStatusChangeReceiver)
+    }
+
+    private fun isInternetConnected(): Boolean {
+        val cm = this.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetwork = cm.activeNetworkInfo ?: return false
+        return activeNetwork.isConnected
     }
 }
